7ccb4916ba980918b54175c54082413e7fd363f6
Gabriel Amorim <gabrielrra.dev@gmail.com>
2021-04-20 18:01:49 -0300

    [INTRO] DONE: Module Welcomer complete

--- lib/inmana/welcomer.ex
+++ lib/inmana/welcomer.ex
@@ -1,19 +1,18 @@
 defmodule Inmana.Welcomer do
-  def welcome(params) do
-    age = params["age"]
-
-    params["name"] |> String.trim() |> String.downcase() |> evaluate(age)
+  def welcome(%{"name" => name, "age" => age}) do
+    age = String.to_integer(age)
+    name |> String.trim() |> String.downcase() |> evaluate(age)
   end
 
   defp evaluate("banana", 42) do
-    "At age 42, you are very special banana"
+    {:ok, "At age 42, you are very special banana"}
   end
 
   defp evaluate(name, age) when age >= 18 do
-    "Welcome #{name}, you are #{age}yo"
+    {:ok, "Welcome #{name}, you are #{age}yo"}
   end
 
   defp evaluate(name, age) do
-    "You shall not pass #{name}! You are only #{age}yo"
+    {:error, "You shall not pass #{name}! You are only #{age}yo"}
   end
 end
