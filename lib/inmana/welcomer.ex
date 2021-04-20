711794857f861fb72936da2d99d21bb4aa99c05d
Gabriel Amorim <gabrielrra.dev@gmail.com>
2021-04-20 17:54:20 -0300

    Initail

--- lib/inmana/welcomer.ex
+++ lib/inmana/welcomer.ex
@@ -1,2 +1,19 @@
-defmodule inmana.Welcomer do
+defmodule Inmana.Welcomer do
+  def welcome(params) do
+    age = params["age"]
+
+    params["name"] |> String.trim() |> String.downcase() |> evaluate(age)
+  end
+
+  defp evaluate("banana", 42) do
+    "At age 42, you are very special banana"
+  end
+
+  defp evaluate(name, age) when age >= 18 do
+    "Welcome #{name}, you are #{age}yo"
+  end
+
+  defp evaluate(name, age) do
+    "You shall not pass #{name}! You are only #{age}yo"
+  end
 end
